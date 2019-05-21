d5719dc53bef7a0d2a283aff3aad6df66367d9c2
julie <julie.bouaye@hotmail.fr>
2019-05-21 17:41:22 +0200

    Changement des modules calendar #9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,12 +25,6 @@ import { routes } from './app.routes';
 import { DeconnexionComponent } from './deconnexion/deconnexion.component';
 import { GestionAbsencesComponent } from './gestion-absences/gestion-absences.component';
 
-/* import du module calendrier */
-import { CommonModule } from '@angular/common';
-import { FlatpickrModule } from 'angularx-flatpickr';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
 	declarations: [
@@ -58,13 +52,6 @@ import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 		HttpClientModule,
 		MDBBootstrapModule.forRoot(),
 		FormsModule,
-		CommonModule,
-		NgbModalModule,
-		FlatpickrModule.forRoot(),
-		CalendarModule.forRoot({
-			provide: DateAdapter,
-			useFactory: adapterFactory
-		})
 	],
 	providers: [{
 		provide: HTTP_INTERCEPTORS,
