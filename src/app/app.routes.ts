5a0d5bcd64400b69f0fd6a482873f668b17879d5
DROUAUD Aurélien <a.drouaud@gmail.com>
2019-05-21 12:19:21 +0200

    Ajout des boutons pour le menu qui seront à trier en fonction de l'utilisateur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { ValidationDemandesComponent } from './validation-demandes/validation-demandes.component';
 import { ManagerVueDptCollabComponent } from './manager-vue-dpt-collab/manager-vue-dpt-collab.component';
 import { JourFerieVisuComponent } from './jour-ferie-visu/jour-ferie-visu.component';
+import { ManagerVueSynthetiqueComponent } from './manager-vue-synthetique/manager-vue-synthetique.component';
 
 
 
@@ -12,6 +13,8 @@ export const routes: Routes = [
 	{ path:'accueil', component: AccueilComponent, canActivate:[StatutConnecteService] },
 	{ path:'gestion-absences', component: ValidationDemandesComponent},
 	{ path:'planning-absences', component: ManagerVueDptCollabComponent},
+	{ path:'validation-demandes', component: ValidationDemandesComponent},
+	{ path:'vues-synthetiques', component: ManagerVueSynthetiqueComponent},
 	{ path:'jour-feries', component: JourFerieVisuComponent},
 	{ path:'auth', component: AuthComponent},
 
