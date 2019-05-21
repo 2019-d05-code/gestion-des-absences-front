8f049234620b9ebd15ec5f943c87a1fc1d7fb409
DROUAUD Aurélien <a.drouaud@gmail.com>
2019-05-21 12:08:22 +0200

    Initialisation du menu Administrateur #3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,20 @@ import { Routes } from '@angular/router';
 import { StatutConnecteService } from './auth/statut-connecte.service';
 import { AuthComponent } from './auth/auth.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ValidationDemandesComponent } from './validation-demandes/validation-demandes.component';
+import { ManagerVueDptCollabComponent } from './manager-vue-dpt-collab/manager-vue-dpt-collab.component';
+import { JourFerieVisuComponent } from './jour-ferie-visu/jour-ferie-visu.component';
 
 
 
 export const routes: Routes = [
 	{ path:'accueil', component: AccueilComponent, canActivate:[StatutConnecteService] },
+	{ path:'gestion-absences', component: ValidationDemandesComponent},
+	{ path:'planning-absences', component: ManagerVueDptCollabComponent},
+	{ path:'jour-feries', component: JourFerieVisuComponent},
 	{ path:'auth', component: AuthComponent},
+
+
 	{ path: '', redirectTo: '/accueil', pathMatch: 'full'}
   ];
 
