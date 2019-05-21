3b4387feaebbd456aa01a059a448b11308923592
julie <julie.bouaye@hotmail.fr>
2019-05-21 14:59:37 +0200

    Correction imports dans appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,10 +26,11 @@ import { DeconnexionComponent } from './deconnexion/deconnexion.component';
 import { GestionAbsencesComponent } from './gestion-absences/gestion-absences.component';
 
 /* import du module calendrier */
+import { CommonModule } from '@angular/common';
+import { FlatpickrModule } from 'angularx-flatpickr';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
 	declarations: [
@@ -57,7 +58,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 		HttpClientModule,
 		MDBBootstrapModule.forRoot(),
 		FormsModule,
-		BrowserAnimationsModule,
+		CommonModule,
+		NgbModalModule,
+		FlatpickrModule.forRoot(),
 		CalendarModule.forRoot({
 			provide: DateAdapter,
 			useFactory: adapterFactory
