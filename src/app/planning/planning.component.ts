9661acdcf94629c3c04f4b8aca7162cb27d80af7
DROUAUD Aurélien <a.drouaud@gmail.com>
2019-05-22 17:12:52 +0200

    Correctif objet calendar non fini

--- src/app/planning/planning.component.ts
+++ src/app/planning/planning.component.ts
@@ -41,21 +41,29 @@ const colors: any = {
 })
 export class PlanningComponent {
 
-	constructor(private _authSrv: AuthService, private _srv: GestionAbsencesService, private _changeRef: ChangeDetectorRef) { }
+	constructor(private _authSrv: AuthService, private _srv: GestionAbsencesService, private _changeRef: ChangeDetectorRef) {
+		this._authSrv.collegueConnecteObs
+			.subscribe(
+				collegue => {
+					this.collegue = collegue;
+				}
+			);
+			console.log(this.collegue.email);
+	}
 
 	view: CalendarView = CalendarView.Month;
 	viewDate = new Date();
-
-	events: CalendarEvent[] = [];
-	collegue: Collegue = new Collegue({});
 
 	evenement: CalendarEvent = {
 		start: startOfDay(new Date()),
 		end: addDays(new Date(), 2),
 		title: `Evenememnt bateau au cas où il y a zéro événèements :p`,
-		color: colors.red,
-		allDay: true,
-	};
+			};
+
+	events: CalendarEvent[]= [this.evenement];
+	collegue: Collegue = new Collegue({});
+
+
 
 	locale = 'fr';
 	weekStartsOn: number = DAYS_OF_WEEK.MONDAY;
@@ -72,17 +80,18 @@ export class PlanningComponent {
 
 	// A l'initialisation, on récupère la liste des absences depuis le back
 	ngOnInit(): void {
-		this.demandeTab = this._srv.getListeAbsences(this.collegue.email);
-
-		this.demandeTab.subscribe(
+		this._srv.getListeAbsences(this.collegue.email).subscribe(
 			demTab => {
 				demTab.map(demande => {
 					this.evenement.start = demande.dateDebut;
 					this.evenement.end = demande.dateFin;
 					this.events.push(this.evenement);
+					console.log(this.events);
+					this._changeRef.detectChanges();
 				});
 			},
 			error => {
+				console.log(error.error);
 				if (error.message === 'Http failure response for http://localhost:8080/listeAbsences: 404 OK') {
 					this.messageErreur = `Pas d'absences disponible`;
 				} else {
@@ -95,14 +104,6 @@ export class PlanningComponent {
 				);
 			}
 		);
-		this._authSrv.collegueConnecteObs
-			.subscribe(
-				collegue => {
-					if (!collegue.estAnonyme()){
-						this.collegue = collegue;
-					}
-				}
-			);
 
 	}
 
