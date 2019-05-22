85e15d475f978db383dc6565b2eee6553333f7e3
Nicolas <marty.nicolas11@laposte.net>
2019-05-22 11:34:19 +0200

    Correction déconnexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "./auth/auth.service";
-import { Router } from "@angular/router";
-import { Observable } from "rxjs/internal/Observable";
 import { Collegue } from "./auth/auth.domains";
+import { Router } from '@angular/router';
 
 /**
  * Composant principal de l'application.
  */
 @Component({
 	selector: 'app-root',
-	templateUrl: `./app.component.html`,
-	styles: []
+	templateUrl: `./app.component.html`
 })
 export class AppComponent implements OnInit {
 
-	constructor() {
+	user: Collegue;
+	messageErreur: string;
+
+	constructor(private _serviceAuth: AuthService, private router: Router) {
 
 	}
 
-	ngOnInit(): void {
+	deconnexion(): void {
+		this._serviceAuth.seDeconnecter().subscribe(
+			() => {
+				this.user = null;
+			},
+			error => {
+				this.messageErreur = error.error;
+				setTimeout(
+					() => this.messageErreur = undefined,
+					7000
+				);
+			}
+		);
+		this.router.navigate(['/connexion']);
+	}
 
+	ngOnInit(): void {
+		this._serviceAuth.collegueConnecteObs.subscribe(
+			collegue => this.user = collegue,
+			error => {
+				this.messageErreur = error.error;
+				setTimeout(
+					() => this.messageErreur = undefined,
+					7000
+				);
+			}
+		);
 	}
 
 }
