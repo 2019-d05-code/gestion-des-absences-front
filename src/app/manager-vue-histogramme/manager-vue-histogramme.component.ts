08bcf3b9c1116a16cd14d524047f2c4f9e39193f
DROUAUD Aurélien <a.drouaud@gmail.com>
2019-05-28 17:30:14 +0200

    Continuation histogramme (non fini)

--- src/app/manager-vue-histogramme/manager-vue-histogramme.component.ts
+++ src/app/manager-vue-histogramme/manager-vue-histogramme.component.ts
@@ -10,79 +10,84 @@ export class ManagerVueHistogrammeComponent implements OnInit {
 	collegueConnecte: Collegue;
 	@Input() connecte: boolean;
 	roleManager: string[];
+
+	// Absence: Absence;
+
+	joursWE: number[];
+	joursMOIS: number[];
+	i: number;
+
+
+
+	// Type du graphique
 	public chartType = 'bar';
 
+	// Jeu de données
 	public chartDatasets: Array<any> = [
-	{ data: [{x: "27/05/2019", y:65}, 59, -0, 81, 56, 55, 40], label: 'My First dataset' },
-	{ data: [{x: "28/05/2019", y:118}, 11, 12, -0, 13, 14, 15, 16], label: 'My Second dataset' }, // Récupérer collègues ici comme dataset
+		{ data: [0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], label: 'this.Absence.nomCollegue1' },
 	];
 
-	public chartLabels: Array<any> = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+	// Pour mettre les 31 colonnes des jours, utiliser CalculJourParMois() à faire
+	public chartLabels: Array<any> = [0, 1, 2, 3, 4, 5, 6];
+
+
 
 	public chartColors: Array<any> = [
 		{
-		backgroundColor: [
-			'rgba(255, 99, 132, 0.2)',
-			'rgba(54, 162, 235, 0.2)',
-			'rgba(255, 206, 86, 0.2)',
-			'rgba(75, 192, 192, 0.2)',
-			'rgba(153, 102, 255, 0.2)',
-			'rgba(255, 159, 64, 0.2)'
-		],
-		borderColor: [
-			'rgba(255,99,132,1)',
-			'rgba(54, 162, 235, 1)',
-			'rgba(255, 206, 86, 1)',
-			'rgba(75, 192, 192, 1)',
-			'rgba(153, 102, 255, 1)',
-			'rgba(255, 159, 64, 1)'
-		],
-		borderWidth: 2,
+			backgroundColor: [
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)',
+				'rgba(255, 159, 64, 0.2)'
+			],
+			borderColor: [
+				'rgba(255,99,132,1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)',
+				'rgba(255, 159, 64, 1)'
+
+			],
+			borderWidth: 2,
 		},
 		{
-		backgroundColor: [
-			'rgba(255, 125, 158, 0.2)',
-			'rgba(3, 111, 184, 0.2)',
-			'rgba(255, 255, 137, 0.2)',
-			'rgba(75, 192, 192, 0.2)',
-			'rgba(126, 243, 243, 0.2)',
-			'rgba(255, 210, 115, 0.2)'
-		],
-		borderColor: [
-			'rgba(255, 125, 158, 1)',
-			'rgba(3, 111, 184, 1)',
-			'rgba(255, 255, 137, 1)',
-			'rgba(75, 192, 192, 1)',
-			'rgba(126, 243, 243, 1)',
-			'rgba(255, 210, 115, 1)'
-		],
-		borderWidth: 2,
+			backgroundColor: [
+				'rgba(255, 125, 158, 0.2)',
+				'rgba(3, 111, 184, 0.2)',
+				'rgba(255, 255, 137, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(126, 243, 243, 0.2)',
+				'rgba(255, 210, 115, 0.2)',
+				'rgba(255, 210, 115, 0.2)'
+			],
+			borderColor: [
+				'rgba(255, 125, 158, 1)',
+				'rgba(3, 111, 184, 1)',
+				'rgba(255, 255, 137, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(126, 243, 243, 1)',
+				'rgba(255, 210, 115, 1)',
+				'rgba(255, 210, 115, 1)'
+
+			],
+			borderWidth: 2,
 		},
-		];
+	];
 
-		public chartOptions: any = {
+	public chartOptions: any = {
 		responsive: true,
-			scales: {
-				xAxes: [{
-					stacked: true,
-					type: 'time',
-					time: {
+		scales: {
+			xAxes: [{
+				scaleLabel: { labelString: 'Dates', display: true },
+				stacked: true,
 
-						displayFormats: {
-							'millisecond' : 'MMM DD',
-							'second' : 'MMM DD',
-							'minute' : 'MMM DD',
-							'hour' : 'MMM DD',
-							'day': 'MMM DD',
-							'week': 'MMM DD',
-							'month': 'MMM DD',
-							'quarter': 'MMM DD',
-							'year': 'MMM DD',
-						},
-						round: 'day',
-						unit: 'day',
-					}
-				}],
+			}],
+
 			yAxes: [{
 				stacked: true,
 
@@ -90,46 +95,57 @@ export class ManagerVueHistogrammeComponent implements OnInit {
 			]
 		}
 
-		};
-		public chartClicked(e: any): void { }
-		public chartHovered(e: any): void { }
+	};
 
-
-		constructor(private _authSrv: AuthService) { }
-
-		verifRoleManager(): boolean {
-	if (this.connecte) {
-
-		let granted = false;
-
-		const roleManager = this.collegueConnecte.roles.filter(role => role === 'ROLE_MANAGER');
-
-		if (roleManager.length > 0) {
-			granted = true;
-
+	CalculJourParMois() {
+		for (this.i = 0; this.i < 32; this.i++) {
+			this.joursMOIS.push(this.i);
+			return this.i;
 		}
-		return granted;
 	}
-}
+
+	CalculWeekendParMois() {
+	}
+
+	public chartClicked(e: any): void { }
+	public chartHovered(e: any): void { }
+
+
+	constructor(private _authSrv: AuthService) { }
+
+	verifRoleManager(): boolean {
+		if (this.connecte) {
+
+			let granted = false;
+
+			const roleManager = this.collegueConnecte.roles.filter(role => role === 'ROLE_MANAGER');
+
+			if (roleManager.length > 0) {
+				granted = true;
+
+			}
+			return granted;
+		}
+	}
 
 
 	ngOnInit() {
-	this._authSrv.collegueConnecteObs
-	.subscribe(
-		collegue => {
-			this.collegueConnecte = collegue;
+		this._authSrv.collegueConnecteObs
+			.subscribe(
+				collegue => {
+					this.collegueConnecte = collegue;
 
-			if (this.collegueConnecte.estAnonyme()) {
-				this.connecte = false;
+					if (this.collegueConnecte.estAnonyme()) {
+						this.connecte = false;
 
-			} else {
-				this.connecte = true;
+					} else {
+						this.connecte = true;
 
-			}
-		},
-		() => this.connecte = false
+					}
+				},
+				() => this.connecte = false
 
-	);
-}
+			);
+	}
 
 }
