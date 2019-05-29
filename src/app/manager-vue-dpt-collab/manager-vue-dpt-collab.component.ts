5ec8e2525131846712f76b1a72cbb03b8aaea175
julie <julie.bouaye@hotmail.fr>
2019-05-29 14:27:02 +0200

    Implémentation tableau (ng for)

--- src/app/manager-vue-dpt-collab/manager-vue-dpt-collab.component.ts
+++ src/app/manager-vue-dpt-collab/manager-vue-dpt-collab.component.ts
@@ -3,50 +3,61 @@ import { Collegue } from '../auth/auth.domains';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
-  selector: 'app-manager-vue-dpt-collab',
-  templateUrl: './manager-vue-dpt-collab.component.html',
-  styles: []
+	selector: 'app-manager-vue-dpt-collab',
+	templateUrl: './manager-vue-dpt-collab.component.html',
+	styles: []
 })
 export class ManagerVueDptCollabComponent implements OnInit {
 	collegueConnecte: Collegue;
 	@Input() connecte: boolean;
 	roleManager: string[];
 
+	// récupérer dernier jour du mois - a implémenter en fonction du mois sélectionné
+	date = new Date(2019, 5, 0);
+	dernierJour = this.date.getDate();
 
-  constructor(private _authSrv: AuthService) { }
+	listeJoursMois: number[] = [];
+
+	constructor(private _authSrv: AuthService) { }
 
 
-  verifRoleManager(): boolean {
-	if (this.connecte) {
+	verifRoleManager(): boolean {
+		if (this.connecte) {
 
-		let granted = false;
+			let granted = false;
 
-		const roleManager = this.collegueConnecte.roles.filter(role => role === 'ROLE_MANAGER');
+			const roleManager = this.collegueConnecte.roles.filter(role => role === 'ROLE_MANAGER');
 
-		if (roleManager.length > 0) {
-			granted = true;
-
-		}
-		return granted;
-	}
-}
-
-  ngOnInit() {
-	this._authSrv.collegueConnecteObs
-	.subscribe(
-		collegue => {
-			this.collegueConnecte = collegue;
-
-			if (this.collegueConnecte.estAnonyme()) {
-				this.connecte = false;
-
-			} else {
-				this.connecte = true;
+			if (roleManager.length > 0) {
+				granted = true;
 
 			}
-		},
-		() => this.connecte = false
+			return granted;
+		}
+	}
 
-	);
-  }
+	ngOnInit() {
+		this._authSrv.collegueConnecteObs
+			.subscribe(
+				collegue => {
+					this.collegueConnecte = collegue;
+
+					if (this.collegueConnecte.estAnonyme()) {
+						this.connecte = false;
+
+					} else {
+						this.connecte = true;
+
+					}
+				},
+				() => this.connecte = false
+
+			);
+
+		// implémentation du tableau en fonction du nombre de jours dans le mois
+		for (let i = 1; i <= this.dernierJour; i++) {
+			this.listeJoursMois.push(i);
+		}
+
+	}
 }
